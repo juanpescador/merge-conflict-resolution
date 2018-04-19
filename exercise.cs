afbcc169e6d5254d046477ab00449548cf566b45
John <juanrobertopescador@gmail.com>
2018-04-19 16:20:13 +0200

    Add static basket total calculation
    As we don't have access to items' prices yet, a hardcoded value of 10 is
    given to each item.

---
+++ exercise.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Math;
+
+namespace Basket
+{
+    public class BasketTotalCalculator
+    {
+        public decimal SubTotal { get; set; }
+
+        public decimal AddBasketItems(Basket basket)
+        {
+            foreach (item in basket.Items)
+            {
+                Subtotal += 10; // Don't have access to item price yet, assume each item costs 10.
+            }
+    }
+}
